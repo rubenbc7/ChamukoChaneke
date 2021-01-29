c7e28ce571e08d8ec0bb730b21e303b15882d394
rubenbc7 <rubenborbollacastelo@gmail.com>
2021-01-29 16:31:36 -0700

    ADmovement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("d")){
+         rb.velocity = transform.right * 5;
+        }
+
+        if (Input.GetKey("a")){
+         rb.velocity = -transform.right * 5;
+        }
+        
+    }
+}
