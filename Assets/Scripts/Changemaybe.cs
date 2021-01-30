e5cf1938afd642059f30446bd9c38e280899baf9
rubenbc7 <rubenborbollacastelo@gmail.com>
2021-01-30 03:30:02 -0700

    mono salta y trans, no colliderbox

---
+++ Assets/Scripts/Changemaybe.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Changemaybe : MonoBehaviour
+{
+  
+  private SpriteRenderer rend;
+  private Sprite TEST, Lamp, Chiken;
+
+  private void Start(){
+      rend = GetComponent<SpriteRenderer>();
+      TEST = Resources.Load<Sprite>("TEST");
+      Lamp = Resources.Load<Sprite>("Lamp");
+      Chiken = Resources.Load<Sprite>("Chiken");
+      rend.sprite = TEST;
+  }
+
+  private void Update(){
+      if(Input.GetMouseButtonDown(0)){
+            if (rend.sprite == Lamp || rend.sprite == Chiken)
+                rend.sprite = TEST;
+            
+      }
+  }
+
+  void OnCollisionEnter (Collision collisionInfo)
+  {
+      Debug.Log(collisionInfo.collider.name);
+      if(collisionInfo.collider.name == "Lamp"){
+          rend.sprite = Lamp;
+      }
+      if(collisionInfo.collider.name =="Chiken"){
+          rend.sprite = Chiken;
+      }
+  }
+
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections;
+﻿  
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
+    public float speed = 15f;
+    
     
     Rigidbody rb;
+    public bool cubeGround = true;
 
     // Start is called before the first frame update
     void Start()
@@ -18,19 +21,35 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
 
-        float xMov = Input.GetAxisRaw("Horizontal");
-        float yMov = Input.GetAxisRaw("Vertical");
+        float xMov = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
+        float yMov = Input.GetAxisRaw("Vertical") * Time.deltaTime * speed ;
 
-        rb.velocity = new Vector3(xMov, rb.velocity.y, yMov) * speed;
+        //rb.velocity = new Vector3(xMov, 0, yMov) * speed;
+        transform.Translate(xMov, 0 ,yMov);
+
+        if(cubeGround == false ){
+            rb.AddForce(new Vector3(0,-1f,0), ForceMode.Impulse);
+        }
+
+        if(Input.GetButtonDown("Jump") && cubeGround){
+             rb.AddForce(new Vector3(0,50f,0), ForceMode.Impulse);
+             cubeGround = false;
+         }
 
 
         /*if (Input.GetKey("d")){
          rb.velocity = transform.right * 5;
         }
-
         if (Input.GetKey("a")){
          rb.velocity = -transform.right * 5;
         }*/
         
     }
+    private void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.name == "Cube"){
+            cubeGround = true;
+        }
+    }
+
+
 }
