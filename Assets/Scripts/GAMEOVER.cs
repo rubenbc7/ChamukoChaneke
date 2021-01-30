0ebd58cdb2fa7b48ff0a885d7c6911b17453c338
DELFIN36 <serpinos17@gmail.com>
2021-01-30 15:44:01 -0600

    GAMEOVER

---
+++ Assets/Scripts/GAMEOVER.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Over : MonoBehaviour
+{
+    public GameObject GameOverText;
+    public static GameObject GameOverStatic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Over.GameOverStatic = GameOverText;
+        Over.GameOverStatic.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public static void show()
+    {
+        Over.GameOverStatic.gameObject.SetActive(true);
+    }
+}
