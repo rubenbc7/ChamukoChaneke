359fc6ae33c9a07277af78b0cc94c9806955f756
Carlos Encinas <carlos-encinas@hotmail.com>
2021-01-29 20:45:10 -0700

    camara y movimiento

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,23 +4,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody rb;
+    public float speed = 5f;
+    
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("d")){
+
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float yMov = Input.GetAxisRaw("Vertical");
+
+        rb.velocity = new Vector3(xMov, rb.velocity.y, yMov) * speed;
+
+
+        /*if (Input.GetKey("d")){
          rb.velocity = transform.right * 5;
         }
 
         if (Input.GetKey("a")){
          rb.velocity = -transform.right * 5;
-        }
+        }*/
         
     }
 }
