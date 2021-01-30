567074d36cf762b0bce88e71758e6eb34139db82
rubenbc7 <rubenborbollacastelo@gmail.com>
2021-01-30 16:17:42 -0700

    Camara sin referencia

--- Assets/Scripts/GAMEOVER.cs
+++ Assets/Scripts/GAMEOVER.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -16,7 +16,7 @@ public class Over : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public static void show()
     {
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,13 +8,13 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(vida<=0)
+        if(life<=0)
         {
             Over.show();
         }
