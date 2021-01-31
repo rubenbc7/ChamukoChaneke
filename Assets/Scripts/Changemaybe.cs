0942c70a847639f066a952b37458c4a7a206f7c4
rubenbc7 <rubenborbollacastelo@gmail.com>
2021-01-30 19:16:16 -0700

    cancion y collison listos

--- Assets/Scripts/Changemaybe.cs
+++ Assets/Scripts/Changemaybe.cs
@@ -7,6 +7,7 @@ public class Changemaybe : MonoBehaviour
   
   private SpriteRenderer rend;
   private Sprite TEST, Lamp, Chiken;
+  public BoxCollider player;
 
   private void Start(){
       rend = GetComponent<SpriteRenderer>();
@@ -14,13 +15,15 @@ public class Changemaybe : MonoBehaviour
       Lamp = Resources.Load<Sprite>("Lamp");
       Chiken = Resources.Load<Sprite>("Chiken");
       rend.sprite = TEST;
+
+      player = player.GetComponent<BoxCollider>();
   }
 
   private void Update(){
       if(Input.GetMouseButtonDown(0)){
             if (rend.sprite == Lamp || rend.sprite == Chiken)
                 rend.sprite = TEST;
-            
+                player.size = new Vector3(11,12.67f,0.5f);   
       }
   }
 
@@ -29,9 +32,11 @@ public class Changemaybe : MonoBehaviour
       Debug.Log(collisionInfo.collider.name);
       if(collisionInfo.collider.name == "Lamp"){
           rend.sprite = Lamp;
+          player.size = new Vector3(7.05f,18.4f,0.5f);
       }
       if(collisionInfo.collider.name =="Chiken"){
           rend.sprite = Chiken;
+          player.size = new Vector3(8.82f,6.5f,1.2f);
       }
   }
 
